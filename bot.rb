bb6f5026437797cdb433339776b3f7432ad525bf
Mike Solomon <msolomon@thrillist.com>
2015-07-17 17:34:53 -0400

    try again

--- bot.rb
+++ bot.rb
@@ -13,8 +13,9 @@ configure do
   Sinatra::Logentries.token = ENV['SINATRA_LOGENTRIES_TOKEN']
 end
 
-class MyHipbot < Hipbot::Bot
+FACTUAL = Factual.new(ENV['FACTUAL_API_KEY'], ENV['FACTUAL_API_SECRET'])
 
+class MyHipbot < Hipbot::Bot
   configure do |c|
     c.jid       = ENV['HIPBOT_JID']
     c.password  = ENV['HIPBOT_PASSWORD']
@@ -41,7 +42,6 @@ class MyHipbot < Hipbot::Bot
 
   def get_recommendation
     random = Random.rand(0...10)
-    FACTUAL = Factual.new(ENV['FACTUAL_API_KEY'], ENV['FACTUAL_API_SECRET'])
     data = FACTUAL.table("places-us").search("coffee").geo("$circle" => {"$center" => [40.7242800, -73.9973540], "$meters" => 500}).include_count(true).rows 
     return data[random]
   end
